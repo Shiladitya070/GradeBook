6febb4338885e2d6833e6a5455391e773e10de91
Shiladitya Das <shiladityad940@gmail.com>
2024-02-02 18:38:47 +0530

    login register

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,8 @@ import { backend_url } from "../../config";
 
 export default function Home() {
   const fn = async () => {
-    const res = await axios.get(`${backend_url}/test`)
+
+    const res = await axios.get(`${backend_url}/ping`)
     console.log(res.data)
   }
   useEffect(() => {
